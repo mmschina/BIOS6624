c7693ebec11f39e396287da308438fd50cf78cc2
mmschina <madelynn.schina@cuanschutz.edu>
2026-04-13 14:26:00 -0600

    Miscellaneous - worksheet updates

--- Worksheets/SurvivalWorksheets.R
+++ Worksheets/SurvivalWorksheets.R
@@ -113,3 +113,69 @@ coxph_fit_ditime <- coxph(Surv(time, status) ~ group + tt(group),
 summary(coxph_fit_ditime)
 
 
+## Survival Worksheet 4
+# Loading the data on ovarian cancer
+ov_data <- read.csv(here("Worksheets", "ovarian.csv"))
+
+# Plots to check PH assumptions
+cox_model <- coxph(Surv(futime, fustat) ~ rx + age + resid.ds + ecog.ps, data = ov_data)
+ph_test <- cox.zph(cox_model)
+ggcoxzph(ph_test)
+
+# Summarizing cox model
+summary(cox_model)
+
+
+# Kaplan Meier Plot
+surv_km <- survfit(Surv(futime, fustat) ~ rx, data = ov_data)
+ggsurvplot(surv_km, data = ov_data, pval = TRUE,
+           legend.labs = c("Treatment 1", "Treatment 2"),
+           title = "Kaplan-Meier Survival Curves by Treatment")
+
+# Estimating and plotting the survival curves for both treatment groups
+ref_data <- data.frame(rx = c(1, 2),
+                       age = mean(ov_data$age),
+                       resid.ds = 1,
+                       ecog.ps = 1)
+
+surv_cox <- survfit(cox_model, newdata = ref_data)
+ggsurvplot(surv_cox, data = ref_data, conf.int = FALSE)
+
+
+# Assessing different ECOG and residual disease factors
+risk_profiles <- data.frame(rx = c(1, 1, 1, 1),
+                            age = mean(ov_data$age),
+                            resid.ds = c(1, 1, 2, 2),
+                            ecog.ps = c(1, 2, 1, 2))
+
+
+surv_profiles <- survfit(cox_model, newdata = risk_profiles)
+summary(surv_profiles, times = 365)
+
+ggsurvplot(surv_profiles, data = risk_profiles,
+           legend.labs = c("resid.ds=1, ecog.ps=1",
+                           "resid.ds=1, ecog.ps=2",
+                           "resid.ds=2, ecog.ps=1",
+                           "resid.ds=2, ecog.ps=2"),
+           conf.int = FALSE)
+
+
+# Survival Curves of two individual patients
+two_patients <- data.frame(rx = c(1, 2),
+                           age = c(60, 55),
+                           resid.ds = c(1, 2),
+                           ecog.ps = c(1, 2))
+
+surv_patients <- survfit(cox_model, newdata = two_patients)
+
+ggsurvplot(surv_patients, data = two_patients,
+           conf.int = FALSE,
+           legend.labs = c("Patient 1", "Patient 2"))
+
+# Probability of death within 1, 1.5 and 2 years
+summary_times <- summary(surv_patients, times = c(365, 547.5, 730))
+death_probs <- data.frame(Patient = c("Patient 1", "Patient 1", "Patient 1", "Patient 2", "Patient 2", "Patient 2"),
+                          Time = c("1 Year", "1.5 Years", "2 Years", "1 Year", "1.5 Years", "2 Years"),
+                          Prob_Death = round(1 - summary_times$surv, 3))
+death_probs
+
