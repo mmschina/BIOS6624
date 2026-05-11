f5ab5cc045363622d98938f7ffffd4cf2d7c7474
mmschina <madelynn.schina@cuanschutz.edu>
2026-05-11 12:39:34 -0600

    Project 4 Final Report - reorganizing code structure and initial desired R files

---
+++ Project 4/Code/RunSimulationsParallel.R
@@ -0,0 +1,18 @@
+#### BIOS 6624 Advanced Methods - Project 4 ####
+## Madelynn Schina
+## May 2026
+
+############################################################
+# The following code runs the simulations in parallel across
+# multiple cores for the sake of decreasing computation time
+############################################################
+
+
+# Loading necessary packages
+library(future)
+library(furr)
+
+
+
+
+
---
+++ Project 4/Code/SimulationFunctions.R
@@ -0,0 +1,38 @@
+#### BIOS 6624 Advanced Methods - Project 4 ####
+## Madelynn Schina
+## May 2026
+
+###########################################################
+# The following code contains a function defined to run the 
+# variable selection simulations required for this project
+###########################################################
+
+
+# Notes
+# "exchangable" correlation; 0, 0.35, 0.7
+# Look into p-value, Carter argued against using 0.157 so 0.05 might be the better choice
+
+
+# Loading necessary packages
+library(hdrm)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
